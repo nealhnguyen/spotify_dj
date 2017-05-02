17d30ee944411f3c8f116af61e42a347a31c865f
vjly <vjly@calpoly.edu>
2017-05-01 17:46:45 -0700

    Added function to get audio feature

--- spotify.py
+++ spotify.py
@@ -4,6 +4,7 @@ GET_ARTIST_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}'
 SEARCH_ENDPOINT = 'https://api.spotify.com/v1/search'
 RELATED_ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/related-artists'
 TOP_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/top-tracks'
+AUDIO_FEATURE = 'https://api.spotify.com/v1/audio-features/{id}'
 
 def get_artist(artist_id):
    url = GET_ARTIST_ENDPOINT.format(id=artist_id)
@@ -25,4 +26,9 @@ def get_artist_top_tracks(artist_id, country='US'):
    url = TOP_TRACKS_ENDPOINT.format(id=artist_id)
    myparams = {'country': country}
    resp = requests.get(url, params=myparams)
+   return resp.json()
+
+def get_audio_features(track_id, country='US'):
+   url = AUDIO_FEATURE.format(id=track_id)
+   resp = requests.get(AUDIO_FEATURE)
    return resp.json()

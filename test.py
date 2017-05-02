39bd6e7d72b296274f1b7903b594462bdb678d96
emlyknguyen <emmmmilyn@pcp129328pcs.wireless.calpoly.edu>
2017-05-02 10:36:18 -0700

    Cleaned up

--- test.py
+++ test.py
@@ -57,28 +57,10 @@ profile1.getDetailedFeatures()
 profile1.edit(7, "tempo", 1.2, "melody", 1.0, "liveliness", 2.3)
 profile1.getDetailedFeatures()
 
-song_ids = []
-for artist in relatedArtists['artists']:
-   #print(artist['name'])
-'''for artist in relatedArtists['artists']:
-   print artist['name']
-   results = spotify.search_by_artist_name(artist['name'])
-   artists = results['artists']
-   artists = artists['items']
-   artistId = artists[0]['id']
-
-   topTracks = spotify.get_artist_top_tracks(artistId, country='US')
-
-   for tracks in topTracks['tracks']:
-     #print('\t' + tracks['name'])
-     #features = spotify.get_audio_features(tracks['id'], country='US')
-     #features = sp.audio_features(tracks['id'])
-     trackID = tracks['id'].encode('utf-8')
-     features = sp.audio_features(trackID)'''
 
 def get_track_features(trackID):
    features = sp.audio_features(trackID)
-   print features
+   print(features)
 
 def get_playlist_tracks(playlistName):
    results = sp.search(q=playlistName, type='playlist')
@@ -88,7 +70,7 @@ def get_playlist_tracks(playlistName):
    results = sp.user_playlist(username, playlist_id)
    for song in results['tracks']['items']:
        #print song['track']['id']
-       print song['track']['name']
+       print(song['track']['name'])
        if not (song['track']['id'] is None):
           track_list.append(song['track']['id'].encode('utf-8'))
 

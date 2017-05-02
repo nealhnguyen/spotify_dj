e7456b8fc3aa44fae81d1b1c2a4fecd14a93323f
vjly <vjly@calpoly.edu>
2017-05-01 17:45:45 -0700

    Audio features

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
+from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import spotify
-
-sp = spotipy.Spotify()
 
 results = spotify.search_by_artist_name('kanye west')
 artists = results['artists']
@@ -10,6 +9,10 @@ artists = artists['items']
 artistId = artists[0]['id']
 relatedArtists = spotify.get_related_artists(artistId)
    
+client_credentials_manager = SpotifyClientCredentials(client_id = 'a64e7ced0f1d40c0960a9f13608c4e37', client_secret = '139896904200432696a1ffda522daa7e')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp.trace=True
+
 for artist in relatedArtists['artists']:
    print artist['name']
    results = spotify.search_by_artist_name(artist['name'])
@@ -17,8 +20,9 @@ for artist in relatedArtists['artists']:
    artists = artists['items']
    artistId = artists[0]['id']
 
-   topTracks = sp.artist_top_tracks(artistId, country='US')
+   topTracks = spotify.get_artist_top_tracks(artistId, country='US')
 
    for tracks in topTracks['tracks']:
-      print '\t' + tracks['name']
-
+#      print '\t' + tracks['name']
+#      features = sp.get_audio_features(tracks['items']['0']['id'])
+      features = sp.audio_features(tracks['items']['0']['id'])

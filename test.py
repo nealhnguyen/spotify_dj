f3216b0f089da68e1114070f2c35dc3cd52ebeb1
Mey Helm <meyhelm@Meys-MacBook-Pro.local>
2017-05-01 22:58:49 -0700

    converted track id from unicode to str

--- test.py
+++ test.py
@@ -9,11 +9,12 @@ artists = artists['items']
 
 artistId = artists[0]['id']
 relatedArtists = spotify.get_related_artists(artistId)
-   
+
 client_credentials_manager = SpotifyClientCredentials(client_id = 'a64e7ced0f1d40c0960a9f13608c4e37', client_secret = '139896904200432696a1ffda522daa7e')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace=True
 
+song_ids = []
 for artist in relatedArtists['artists']:
    print artist['name']
    results = spotify.search_by_artist_name(artist['name'])
@@ -24,7 +25,8 @@ for artist in relatedArtists['artists']:
    topTracks = spotify.get_artist_top_tracks(artistId, country='US')
 
    for tracks in topTracks['tracks']:
-      print tracks['id']
-#      print '\t' + tracks['name']
-#      features = spotify.get_audio_features(tracks['id'], country='US')
-      features = sp.audio_features(tracks['id'])
+     #print '\t' + tracks['name']
+     #features = spotify.get_audio_features(tracks['id'], country='US')
+     #features = sp.audio_features(tracks['id']
+     trackID = tracks['id'].encode('utf-8')
+     features = sp.audio_features(trackID)

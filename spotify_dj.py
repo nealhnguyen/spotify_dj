3433ae579d47a8c21349a9ae858180fd6868af7f
vjly <vjly@calpoly.edu>
2017-06-05 16:51:59 -0700

    fixed data and spotify dj

--- spotify_dj.py
+++ spotify_dj.py
@@ -9,6 +9,7 @@ requiredSim = .990
 sugg_limit = 5
 tracksPerList = 5
 sp = classes.Spotify()
+suggestedTracks = []
 
 def get_field(field_name, list_values):
    return float(list_values[list_values.index(field_name) + 1])
@@ -18,11 +19,12 @@ def extract_feat_dict(csv_entry, features):
    feature_dict = {}
 
    name = list_values[0]
+   playlist_id = list_values[1]
 
    for feature in features:
       feature_dict[feature] = get_field(feature, list_values)
 
-   return name, feature_dict
+   return name, playlist_id, feature_dict
 
 def build_playlist(playlist_name, top_features):
    #user = Playlist("Pop Rising", sp, {"danceability": 0.6693300000000002, "acousticness": 0.13855849999999992, "energy": 0.6927300000000003, "liveness": 0.17840999999999993, "valence": 0.495797})
@@ -39,14 +41,14 @@ def build_playlist(playlist_name, top_features):
    return user
 
 def build_sugg_playlists(database_file, top_features):
-   playlists = []
+   playlists = {} 
 
    with open(database_file, 'rb') as csvFile:
       for row in csvFile:
-         name, features = extract_feat_dict(row, top_features)
+         name, playlist_id, features = extract_feat_dict(row, top_features)
          curr_playlist = Playlist(name, sp, features)
 
-         playlists.append(curr_playlist)
+         playlists[playlist_id] = curr_playlist
 
       return playlists
 
@@ -57,14 +59,15 @@ def suggest_songs(user, playlist, required_sim, sugg_limit, weight):
    track_ids, track_names = sp.get_playlist_tracks(playlist_user, playlist_id)
    track_features = sp.get_features(track_ids)
 
-   for track_name, track_feature in izip(track_names, track_features):
+   for track_name, track_id, track_feature in izip(track_names, track_ids, track_features):
       track_sub_features = get_sub_dict(user.get_feature_keys(), track_feature)
       curr_track = Song(track_sub_features)
 
       sim = Profile.compare(user, curr_track, weight)
 
-      if  sim >= required_sim:
+      if sim >= required_sim and track_id not in suggestedTracks:
          songs_matched += 1
+         suggestedTracks.append(track_id)
          print "   ", track_name, " ", sim
 
       if songs_matched >= sugg_limit:
@@ -80,12 +83,11 @@ def spotify_dj():#playlist_name, top_features, weight):
 
    playlists = build_sugg_playlists('data.csv', top_features)
 
-   for playlist in playlists:
+   for playlist_id, playlist in playlists.items():
       similarity = Profile.compare(user, playlist, weight)
 
-      if similarity > requiredSim:
+      if similarity > requiredSim and playlist_id != user.uri:
          print playlist.name, similarity
-
          suggest_songs(user, playlist, requiredSim, sugg_limit, weight)
 
 if __name__ == '__main__':

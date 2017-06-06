39e7d9f1d4cfbb684a00a43e52eca502426a9491
emlyknguyen <emmmmilyn@pcp129533pcs.wireless.calpoly.edu>
2017-06-05 17:24:51 -0700

    Separate ui file

--- mey.py
+++ mey.py
@@ -2,33 +2,38 @@ import test
 import classes
 
 def UserInput():
-
+   
    sp = classes.Spotify()
 
-   playlist = raw_input('Enter a playlist: ')
-
-   check = sp.search_playlist(playlist) #checking if the playlist exists
-
+   user_playlist = raw_input('Enter a playlist name:')
+   
+   check = sp.search_playlist(user_playlist) #checking if the playlist exists
+   
    while check == -1:
-       playList = raw_input('Enter a valid playlist: ')
-       check = sp.search_playlist(playlist)
-
+      playList = raw_input('Enter a valid playlist: ')
+      check = sp.search_playlist(user_playlist)
+   
    print("Enter the feature's corresponding numbers to value (on a scale of 1-9) separated by commas: ")
-   print("For example, Dancability with the value 8 would be 5:8")
-   print("      (1) - Energy\n      (2) - Liveness\n      (3) - Tempo")
-   print("      (4) - Acousticness\n      (5) - Dancability\n      (6) - Valence")
+   print("  Ex. Dancability with the value 8 would be 5:8")
+   print("      (1) - Energy\n      (2) - Liveness")
+   print("      (3) - Acousticness\n      (4) - Dancability\n      (5) - Valence")
    features = raw_input('')
    features = features.replace(' ', '')
    chosen_features = features.split(",")
-   print chosen_features
-
+   
    features_dict = dict(map(lambda s : s.split(':'), chosen_features))
    for key, value in features_dict.items():
-       features_dict[key] = int(value)
+      features_dict[key] = int(value)
 
-   print(features_dict)
-   #profile = test.makeProfile(playlist, "danceability", "acousticness", "valence", "liveness", "tempo", "energy")
-   #print profile.getDetailedFeatures()
+
+
+   userPreferredFeat = {}
+
+   for key, value in features_dict.items():
+      featName = numToFeature[key]
+      userPreferredFeat[featName] = int(value)
+
+   return user_playlist, userPreferredFeat
 
 if __name__ == '__main__':
    UserInput()

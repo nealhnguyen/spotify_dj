d4922809d0d37c4d95d32fd7d2e535b7b0ff2a2f
Neal Nguyen <nealhnguyen@gmail.com>
2017-06-05 17:57:24 -0700

    now default value for feature if not specified

---
+++ user_input.py
@@ -0,0 +1,35 @@
+import test
+import classes
+
+numToFeature = {'1':"energy", '2':"liveness", '3':"acousticness", '4':"danceability", '5':"valence"}
+
+def UserInput():
+   sp = classes.Spotify()
+
+   user_playlist = raw_input('Enter a playlist name:')
+
+   check = sp.search_playlist(user_playlist) #checking if the playlist exists
+
+   while check == -1:
+      playList = raw_input('Enter a valid playlist: ')
+      check = sp.search_playlist(user_playlist)
+
+   print("Enter the feature's corresponding numbers to value (on a scale of 1-9) separated by commas: ")
+   print("  Ex. Dancability with the value 8 would be 5:8")
+   print("      (1) - Energy\n      (2) - Liveness")
+   print("      (3) - Acousticness\n      (4) - Dancability\n      (5) - Valence")
+   features = raw_input('')
+   features = features.replace(' ', '')
+   chosen_features = features.split(",")
+
+   features_dict = dict(map(lambda s : s.split(':'), chosen_features))
+   userPreferredFeat = dict.fromkeys(['energy', 'liveness', 'acousticness', 'danceability', 'valence'], 5)
+
+   for key, value in features_dict.items():
+      featName = numToFeature[key]
+      userPreferredFeat[featName] = int(value)
+
+   return user_playlist, userPreferredFeat
+
+if __name__ == '__main__':
+   UserInput()
